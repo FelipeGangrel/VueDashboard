23f07ad7f84981030ef639c6abfc32cce3df1fb4
Felipe Gangrel <felipefrog@gmail.com>
2018-11-11 02:26:51 -0200

    update

--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -2,4 +2,13 @@
   #Home
     b-container(fluid)
       b-card(title="Home")
+        b-row
+          b-col
+            p Labore consequat cupidatat id consequat proident in reprehenderit. Dolor nulla Lorem cupidatat officia sit. Deserunt tempor duis elit consequat voluptate aute esse sunt.
+        b-row
+          b-col
+            p Labore consequat cupidatat id consequat proident in reprehenderit. Dolor nulla Lorem cupidatat officia sit. Deserunt tempor duis elit consequat voluptate aute esse sunt.
+            b-button(variant="outline-primary" size="lg" block) botão
+          b-col
+            p Labore consequat cupidatat id consequat proident in reprehenderit. Dolor nulla Lorem cupidatat officia sit. Deserunt tempor duis elit consequat voluptate aute esse sunt.
 </template>
